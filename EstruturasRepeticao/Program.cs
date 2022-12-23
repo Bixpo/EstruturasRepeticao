979be96277557772c11f09c702efc7874257f996
Bixpo <bispobispocruz@gmail.com>
2022-12-23 08:16:42 -0300

    EstruturasRepeticao

---
+++ EstruturasRepeticao/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EstruturasRepeticao
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 5)
+            //        // continue;
+            //        break;
+            //    Console.WriteLine(i);
+            //}
+
+            //foreach (var item in DriveInfo.GetDrives())
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine(i);
+                continue;
+                break;
+                i++;
+            } while (i < 0);
+
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
